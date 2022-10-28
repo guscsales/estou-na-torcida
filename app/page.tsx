605a861cc4f68e4cd9878186d654657f639ba0bd
Gustavo Sales <gustavo@gsales.io>
2022-10-28 20:53:06 -0300

    feat(og): creating twitter image

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,60 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Poppins } from '@next/font/google';
+
+const font = Poppins({ weight: '700' });
+const fontItalic = Poppins({ weight: '700', style: 'italic' });
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+    <div
+      className={`
+        ${font.className} lg:w-[1200px] h-[628px] mx-auto mt-28 
+      `}
+    >
+      <div
+        style={{
+          display: 'flex',
+          backgroundImage: `url(/images/players/neymar-twitter.png)`,
+        }}
+        className={`w-[1200px] h-[628px] bg-no-repeat bg-bottom flex flex-col justify-end pb-7 pl-14`}
+      >
+        <div className="flex gap-2 items-center">
+          <div
+            className="rounded-full w-20 h-20 border-2 border-solid border-white p-0.5 overflow-hidden"
+            aria-label="Foto de Gustavo"
           >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://ca.slack-edge.com/T0259JYFTTK-U02L9EWAA10-2f5925353804-512"
+              className="flex rounded-full"
+              alt="Foto de Gustavo"
+            />
+          </div>
+          <div className="flex flex-col w-[270px]">
+            <span
+              className="text-2xl drop-shadow-md text-white"
+              style={{
+                filter:
+                  'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06));',
+              }}
+            >
+              Gustavo Sales
+            </span>
+            <span
+              className={`${fontItalic.className} text-lg text-amber-300`}
+              style={{
+                filter:
+                  'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06));',
+              }}
+            >
+              #EstouNaTorcida pela Seleção Braseileira no Catar
+            </span>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <h1 className="text-white text-8xl w-8/12 my-7 drop-shadow-green">
+          Vai Brasil! Rumo ao Hexa!
+        </h1>
+        <div className="w-[134px] h-[32px] bg-no-repeat bg-[url(/images/fifa-qatar-logo.png)]" />
+      </div>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['app/**/*.tsx'],
+  theme: {
+    extend: {
+      dropShadow: {
+        green: '2px 2px 1px #047857',
+      },
+    },
+  },
+  plugins: [],
+};
