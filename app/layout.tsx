3152fae5d45003c4fb3ad81884715980d91f837e
Gustavo Sales <gustavo@gsales.io>
2022-11-02 10:56:07 -0300

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <meta name="viewport" content="width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
